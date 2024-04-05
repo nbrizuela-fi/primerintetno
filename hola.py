c526d7191e399694d20e1e8c279d90ab1f749eb1
PC-escr <nbrizuela@fi.uba.ar>
2024-04-05 23:55:36 +0000

    cambios en olas's

--- hola.py
+++ hola.py
@@ -1,2 +1,12 @@
-for i in range(10):
-    print(f"holaaa mundo {i}")
+def funcion1(rango: str):
+    for i in range(rango):
+        print(f"holaaa mundo {i}")
+
+
+def main():
+    rango: int = int(input("ingresa un rago: "))
+
+    funcion1(rango)
+
+
+main()
