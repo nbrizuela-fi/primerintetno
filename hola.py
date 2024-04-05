724284b4e2daf8e14e5e81427e97e27eaf6d8b20
PC-escr <nbrizuela@fi.uba.ar>
2024-04-05 23:49:53 +0000

    modificacion al ola desde main

--- hola.py
+++ hola.py
@@ -1,2 +1,18 @@
-for i in range(10):
-    print(f"holaaa mundo {i}")
+def saludo_10():
+    for i in range(10):
+        print(f"holaaa mundo {i}")
+
+
+def saluda_con_nombre(nombre: str, iterado: int):
+    for i in range(iterado):
+        print(nombre)
+
+
+def main():
+    nombre = input("ingresa tu nombre: ")
+
+    cantidad = int(input("ingresa las veces de tu sludo: "))
+    saluda_con_nombre(nombre, cantidad)
+
+
+main()
